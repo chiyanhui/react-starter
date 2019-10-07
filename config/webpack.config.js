2e06abce40bb4949d42b6b222748a85ffbea5058
chiyanhui <chiyanhui@126.com>
2019-10-07 16:56:17 +0800

    使用html-webpack-plugin处理html

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 process.env.BABEL_ENV = 'production';
 module.exports = {
@@ -17,6 +18,10 @@ module.exports = {
     ],
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      favicon: 'public/favicon.ico',
+    }),
   ],
   devServer: {
     contentBase: './public',
